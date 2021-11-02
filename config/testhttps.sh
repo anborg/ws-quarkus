6516be5898a71526597de4f6ff1626000fe5db38
test <test@model.com>
2021-11-02 17:59:30 -0400

    test

--- config/testhttps.sh
+++ config/testhttps.sh
@@ -1,3 +1,4 @@
+BASEDIR=$(dirname $0)
 #-v
 URL1='https://localhost:8043/api/v1/asset/health'
 echo $URL1
@@ -5,9 +6,9 @@ curl -k  $URL1
 echo
 URL2='https://localhost:8043/api/v1/asset/health/secure'
 echo $URL2
-curl -k --netrc-file testhttpspwd.txt  $URL2
+curl -k --netrc-file $BASEDIR/testhttpspwd.txt  $URL2
 echo
 URL3='https://localhost:8043/api/v1/asset/workorders/1'
 echo $URL3
-curl -k --netrc-file testhttpspwd.txt  $URL3
+curl -k --netrc-file $BASEDIR/testhttpspwd.txt  $URL3
 echo
